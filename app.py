723e818633bcbc319dbfad14d071a2debf06fa86
xaiguy <p.biese@gmx.net>
2023-02-15 23:04:33 +0100

    Switched to chatbot interface with a custom theme

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 # Imports
 import streamlit as st
+from streamlit_chat import message
 import requests
+
+import regex as re
 
 st.title("Chippy FastAPI")
 
@@ -11,7 +14,22 @@ data = {
     "input_prompt": input_prompt,
 }
 
-# Generate output
-if st.button("Chip it!"):
-    res = requests.post("http://localhost:8000/predict/", json=data)
-    st.markdown(res.json())
+placeholder = st.empty()  # placeholder for latest message
+message_history = []
+message_history.append(input_prompt)
+
+for j, message_ in enumerate(message_history):
+    if j % 2 == 0:
+        message(message_, is_user=True) # display all the previous message
+
+#with placeholder.container():
+#    message(message_history[-1]) # display the latest message
+
+res = requests.post("http://localhost:8000/predict/", json=data)
+cleaned_answer = re.sub("User:.+\n+Chip: ", "", res.json())
+message(cleaned_answer)
+
+## Generate output
+#if st.button("Chip it!"):
+#    res = requests.post("http://localhost:8000/predict/", json=data)
+#    st.markdown(res.json())
